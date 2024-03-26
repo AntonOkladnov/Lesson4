125b2fe55d1061cffab3ea9911cc1d1c16321f38
Anton <playanton21@rambler.ru>
2024-03-26 18:37:28 +0300

    lesson4:add task3

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.Readline();
+string result = GetLettersFromString(str);
+System.Console.WriteLine(result);
